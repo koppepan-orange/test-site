5da98f6a8a7c00d6f0a158bbe8496c052d2a74ca
koppepan_orange <165629784+koppepan-orange@users.noreply.github.com>
2024-05-15 06:52:31 +0900

    Create homepage_COUNT.js

---
+++ homepage_COUNT.js
@@ -0,0 +1,70 @@
+let COUNTx = 0;
+let COUNTope = 0;
+let COUNTgamebar = 0;
+const COUNTgamebars = ['##@','####@','######@','########@','##########@','############@','##############@'];
+const COUNTTips = ['パン工場〜','だから愛だよ','ひとえに、愛だよ','うにょ〜ん','異議あり!','ウロボロスの弟、ウヌボロス','もろたで四輪工藤','小籠包と小論文のハーフ、ショウロンプォゥン','なんだかすごそう建武の新政'];
+function COUNTGameStart(){
+    COUNTgamebar = COUNTgamebars[Math.floor(Math.random() * COUNTgamebars.length)];
+    document.getElementById("COUNTGameBar").textContent = COUNTgamebar;
+    document.getElementById("COUNTButton").innerHTML = '<button onclick="COUNTMove1()">1</button>   <button onclick="COUNTMove2()">2</button>';
+    COUNTx = COUNTgamebar.length - 1;
+    document.getElementById("COUNTLog").textContent = 'gamestart!今回は' + COUNTx + 'の長さです!!';
+    COUNTope = 1;
+}
+async function COUNTMove1(){
+    if(COUNTope == 1){
+    COUNTope = 0;
+    document.getElementById("COUNTButton").innerHTML = '';
+    document.getElementById("COUNTButton").innerHTML = '<button onclick="COUNTMove1()">1</button>   <button onclick="COUNTMove2()">2</button>';
+    if (COUNTgamebar.length == 1){
+    COUNTGameOver();
+    COUNTope = 0;
+    } else {
+      COUNTgamebar = COUNTgamebar.slice( 1 );
+      document.getElementById("COUNTGameBar").textContent = COUNTgamebar;
+      COUNTx -= 1;
+      document.getElementById("COUNTLog").textContent = '1が取り除かれ,残りは' + COUNTx + '個になりました!';
+      await new Promise(COUNTresolve => setTimeout(COUNTresolve, 300));
+      COUNTope = 1;
+      document.getElementById("COUNTButton").innerHTML = '<button onclick="COUNTMove1()">1</button>   <button onclick="COUNTMove2()">2</button>';
+    }
+    }
+  }
+async function COUNTMove2(){
+    if(COUNTope == 1){
+    COUNTope = 0;
+    document.getElementById("COUNTButton").innerHTML = '';
+    if (COUNTgamebar.length == 1){
+    COUNTope = 0;
+    COUNTGameOver();
+    } else {
+    COUNTgamebar = COUNTgamebar.slice( 1 );
+    document.getElementById("COUNTGameBar").textContent = COUNTgamebar;
+    COUNTx -= 1;
+    document.getElementById("COUNTLog").textContent = '1が取り除かれ,残りは' + COUNTx + '個になりました!';
+    await new Promise(COUNTresolve => setTimeout(COUNTresolve, 300));
+    if (COUNTgamebar.length == 1){
+      COUNTope = 0;
+      COUNTGameOver();
+    } else {
+      COUNTgamebar = COUNTgamebar.slice( 1 );
+      document.getElementById("COUNTGameBar").textContent = COUNTgamebar;
+      COUNTx -= 1;
+      document.getElementById("COUNTLog").textContent = '1が取り除かれ,残りは' + COUNTx + '個になりました!';
+      await new Promise(COUNTresolve => setTimeout(COUNTresolve, 300));
+      COUNTope = 1;
+      document.getElementById("COUNTButton").innerHTML = '<button onclick="COUNTMove1()">1</button>   <button onclick="COUNTMove2()">2</button>';
+      }
+    }
+    }
+  }
+function COUNTGameOver(){
+    document.getElementById("COUNTLog").textContent = 'gameover!';
+    document.getElementById("COUNTButton").innerHTML = '<button onclick="COUNTGameReset()">reset</button>';
+    }
+function COUNTGameReset(){
+    document.getElementById("COUNTGameBar").textContent = '';
+    document.getElementById("COUNTLog").textContent = COUNTTips[Math.floor(Math.random() * COUNTTips.length)];
+    document.getElementById("COUNTButton").innerHTML = '<button onclick="COUNTGameStart()">start</button>';
+    COUNTx = 0;
+    }
