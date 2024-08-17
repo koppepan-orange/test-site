d7aa6150ed34cb2cc788aaf0c5c1dae18b260519
koppepan_orange <165629784+koppepan-orange@users.noreply.github.com>
2024-08-18 07:30:29 +0900

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -12,6 +12,27 @@ function  HENsum(){
   HENanswer = Math.floor(HENy + 50);
   document.getElementById("HENAnswer").textContent = '偏差値は' + HENanswer + 'です';
 }
+//合言葉の動き
+function showOutput() {
+    const input = document.getElementById('inputText').value;
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = ''; // 前の出力をクリアする
+  
+    let redirectUrl;
+    if(input.includes('gakkoudaisuki')) {
+      redirectUrl = 'https://true-koppepan-orange.github.io/sub_test_site/meibo_site';
+    } else if(input.includes('gamegasitaidesu')) {
+      redirectUrl = 'https://koppepan-orange-game.github.io/game_daisuki/welcome_page.html';
+    } else {
+      'パスワードが違います。';
+    }
+  
+    const link = document.createElement('a');
+    link.href = redirectUrl;
+    link.textContent = 'success!';
+    link.target = '_blank'; // 新しいタブで開く
+    outputDiv.appendChild(link);
+}
 //CountGame = COUNT
 let COUNTx = 0;
 let COUNTope = 0;
